c33644293f27c11b479dbe8c5bad651b385f81b9
sijoyalmeida09 <144766943+sijoyalmeida09@users.noreply.github.com>
2023-12-02 21:15:30 -0500

    Review Request, Review Service, Review Controller

---
+++ src/main/java/com/BookWave/springbootlibrary/controller/ReviewController.java
@@ -0,0 +1,30 @@
+package com.BookWave.springbootlibrary.controller;
+
+import com.BookWave.springbootlibrary.requestmodels.ReviewRequest;
+import com.BookWave.springbootlibrary.service.ReviewService;
+import org.springframework.web.bind.annotation.*;
+import utils.ExtractJWT;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+
+
+public class ReviewController {
+
+    private ReviewService reviewService;
+
+    public ReviewController (ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value="Authorization") String token,
+                           @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
