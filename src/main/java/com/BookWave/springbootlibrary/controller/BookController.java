e8f4fad3e1ed86153b3ea00de571ede80c01ea36
chawla-si <chawla.si@northeastern.edu>
2023-12-03 14:12:05 -0500

    Add springboot security and JWT token authorization

--- src/main/java/com/BookWave/springbootlibrary/controller/BookController.java
+++ src/main/java/com/BookWave/springbootlibrary/controller/BookController.java
@@ -4,6 +4,7 @@ import com.BookWave.springbootlibrary.entity.Book;
 import com.BookWave.springbootlibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import utils.ExtractJWT;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -18,20 +19,20 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "sijoyalmeida@gmail.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId) {
-        String userEmail = "sijoyalmeida@gmail.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook (@RequestParam Long bookId) throws Exception {
-        String userEmail = "sijoyalmeida@gmail.com";
+    public Book checkoutBook (@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
